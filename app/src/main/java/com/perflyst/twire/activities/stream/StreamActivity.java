aba0ab952c4e0112750e7ee6b25c3be6336c5d60
MusseJusse <mussejusse@gmail.com>
2021-10-20 23:15:53 +0100

    Removed chat fragment inflation

--- app/src/main/java/com/perflyst/twire/activities/stream/StreamActivity.java
+++ app/src/main/java/com/perflyst/twire/activities/stream/StreamActivity.java
@@ -87,10 +87,10 @@ public abstract class StreamActivity extends ThemeActivity implements SensorEven
                 fm.beginTransaction().replace(getVideoContainerResource(), mStreamFragment, getString(R.string.stream_fragment_tag)).commit();
             }
 
-            if (mChatFragment == null) {
+            /*if (mChatFragment == null) {
                 mChatFragment = ChatFragment.getInstance(getStreamArguments());
                 fm.beginTransaction().replace(R.id.chat_fragment, mChatFragment).commit();
-            }
+            }*/
         }
 
         try {
